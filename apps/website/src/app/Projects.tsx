1afb57107d8321182a298113c6fd35d6b4f09861
Grace Durant <gdurant34@gmail.com>
2023-06-22 07:10:18 -0700

    beginning project structure

--- apps/website/src/app/Projects.tsx
+++ apps/website/src/app/Projects.tsx
@@ -1,16 +1,18 @@
 import { Tab } from '@grace-website/components';
 
 const Projects = ({ title }: { title: string }) => {
-    return (
-        <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] h-screen">
+  
+
+  return (
+    <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] h-screen">
       <img
         src="/assets/images/annie-spratt-PM4Vu1B0gxk-unsplash.jpg"
         alt=""
         className="w-full h-full z-0 object-cover absolute mix-blend-overlay opacity-90"
       />
       <Tab />
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Projects;
