c06d1d0e9b07780204d5191b33cd7f880a90f3a3
Grace Durant <gdurant34@gmail.com>
2023-07-10 00:46:38 -0700

    fixed tabs and project page for mobile

--- libs/components/src/lib/tab.tsx
+++ libs/components/src/lib/tab.tsx
@@ -2,79 +2,73 @@ import { Root, List, Trigger, Content } from '@radix-ui/react-tabs';
 import tabs from '../lib/projects.json';
 import { useState } from 'react';
 
-const active =
-  'bg-amber-800 bg-opacity-5 shadow-sm text-cyan-700 px-4 py-3 flex items-center space-x-4 rounded-md font-bold';
-const inactive =
-  'relative px-4 py-3 flex items-center space-x-4 text-gray-600 group rounded-md hover:bg-teal-500 hover:bg-opacity-10';
+const active = 'bg-amber-800 bg-opacity-5 shadow-sm text-cyan-700 px-4 py-3 flex items-center space-x-4 rounded-md font-bold';
+const inactive = 'relative px-4 py-3 flex items-center space-x-4 text-gray-600 group rounded-md hover:bg-teal-500 hover:bg-opacity-10';
 
 export const Tab = () => {
   const [currentValue, setCurrentValue] = useState('tab1');
 
   return (
-    <Root defaultValue="tab1" onValueChange={setCurrentValue}>
-      <div className="flex justify-center p-6 ">
-        <div className="z-20 relative text-center text-cyan-900 bg-stone-100 bg-opacity-50 rounded-sm text-lg w-11/12">
-          <List
-            className="p-8 flex justify-center"
-            aria-label="Manage your account"
-          >
+    <div className="container mx-auto px-4">
+      <Root defaultValue="tab1" onValueChange={setCurrentValue}>
+        <div className="p-6 flex justify-center">
+          <div className="z-20 relative text-cyan-900 bg-stone-100 bg-opacity-50 rounded-sm text-lg sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl">
+            <List className="p-8 flex justify-center" aria-label="Manage your account">
+              {tabs.map((tab) => (
+                <Trigger
+                  key={tab.id}
+                  className={`px-4 ${tab.value === currentValue ? active : inactive}`}
+                  value={tab.value}
+                >
+                  {tab.title}
+                </Trigger>
+              ))}
+            </List>
             {tabs.map((tab) => (
-              <Trigger
-                key={tab.id}
-                className={`px-4 ${
-                  tab.value === currentValue ? active : inactive
-                }`}
-                value={tab.value}
-              >
-                {tab.title}
-              </Trigger>
-            ))}
-          </List>
-          {tabs.map((tab) => (
-            <Content className="px-4" value={tab.value} key={tab.id}>
-              <p className="italic text-amber-800 text-opacity-70 flex justify-center text-lg p-4">
-                {tab.description}
-              </p>
-              <div className="w-full">
-                <ul className="text-base w-full p-4 justify-start list-inside">
-                  {tab.bullets.map((bullet) => (
-                    <li key={bullet} className="p-2 w-full">
-                      {bullet}
-                    </li>
-                  ))}
-                </ul>
-                <div className="aspect-w-16 aspect-h-9 flex justify-center">
-                  <iframe
-                    className="m-5"
-                    width="560"
-                    height="315"
-                    src={tab.youtube}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-                <div className="m-4 flex justify-center">
-                  {tab.github.map((link) => (
-                    <li
-                      key={link.description}
-                      className="m-4 flex justify-center list-none w-1/3 rounded border-2 border-cyan-900 text-opacity-90 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-cyan-900 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-stone-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                    >
-                      <a
-                        href={link.link}
-                        className="text-cyan-900 text-opacity-90"
+              <Content className="px-4" value={tab.value} key={tab.id}>
+                <p className="italic text-amber-800 text-opacity-70 flex justify-center text-lg p-4">
+                  {tab.description}
+                </p>
+                <div className="w-full">
+                  <ul className="text-base px-40 list-inside">
+                    {tab.bullets.map((bullet) => (
+                      <li key={bullet} className="p-2">
+                        {bullet}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="aspect-w-16 aspect-h-9 flex justify-center">
+                    <div className="w-1/2 h-1/2">
+                      <iframe
+                        className="m-5"
+                        width="100%"
+                        height="100%"
+                        src={tab.youtube}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  </div>
+                  <div className="m-4 flex justify-center space-x-4">
+                    {tab.github.map((link) => (
+                      <li
+                        key={link.description}
+                        className="m-4 flex justify-center list-none w-1/3 rounded border-2 border-cyan-900 text-opacity-90 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-cyan-900 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-stone-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                       >
-                        {link.description}
-                      </a>
-                    </li>
-                  ))}
+                        <a href={link.link} className="text-cyan-900 text-opacity-90">
+                          {link.description}
+                        </a>
+                      </li>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </Content>
-          ))}
+              </Content>
+            ))}
+          </div>
         </div>
-      </div>
-    </Root>
+      </Root>
+    </div>
   );
 };
 
