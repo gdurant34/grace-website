bbd397f44603afff3882dc95bb1fcfaf35582f5f
Grace Durant <gdurant34@gmail.com>
2023-09-25 13:10:40 -0700

    added new project to projects

--- libs/components/src/lib/tab.tsx
+++ libs/components/src/lib/tab.tsx
@@ -2,22 +2,62 @@ import { Root, List, Trigger, Content } from '@radix-ui/react-tabs';
 import tabs from '../lib/projects.json';
 import { useState } from 'react';
 
-const active = 'bg-amber-800 bg-opacity-5 shadow-sm text-cyan-700 px-4 py-3 flex items-center space-x-4 rounded-md font-bold';
-const inactive = 'relative px-4 py-3 flex items-center space-x-4 text-gray-600 group rounded-md hover:bg-teal-500 hover:bg-opacity-10';
+const active =
+  'bg-amber-800 bg-opacity-5 shadow-sm text-cyan-700 px-4 py-3 flex items-center space-x-4 rounded-md font-bold';
+const inactive =
+  'relative px-4 py-3 flex items-center space-x-4 text-gray-600 group rounded-md hover:bg-teal-500 hover:bg-opacity-10';
 
 export const Tab = () => {
   const [currentValue, setCurrentValue] = useState('tab1');
+
+  // const website = tab.websiteInfo ? (
+  //   <div className="m-4 flex justify-center space-x-4">
+  //     {tab.websiteInfo.map((link) => (
+  //       <li
+  //         key={link.description}
+  //         className="m-4 flex justify-center list-none w-1/3 rounded border-2 border-cyan-900 text-opacity-90 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-cyan-900 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-stone-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+  //       >
+  //         <a href={link.link} className="text-cyan-900 text-opacity-90">
+  //           {link.description}
+  //         </a>
+  //       </li>
+  //     ))}
+  //   </div>
+  // ) : null;
+
+  const website = tabs.map((tab) => {
+    return tab.websiteInfo != null && tab.value === currentValue ? (
+      <div className="m-4 flex justify-center space-x-4">
+        <li
+          key={tab.id}
+          className="m-4 flex justify-center list-none w-1/3 rounded border-2 border-cyan-900 text-opacity-90 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-cyan-900 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-stone-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+        >
+          <a
+            href={tab.websiteInfo[0].link}
+            className="text-cyan-900 text-opacity-90"
+          >
+            {tab.websiteInfo[0].description}
+          </a>
+        </li>
+      </div>
+    ) : null;
+  });
 
   return (
     <div className="container mx-auto px-4">
       <Root defaultValue="tab1" onValueChange={setCurrentValue}>
         <div className="p-6 flex justify-center">
           <div className="z-20 relative text-cyan-900 bg-stone-100 bg-opacity-50 rounded-sm text-lg sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl">
-            <List className="p-8 flex justify-center" aria-label="Manage your account">
+            <List
+              className="p-8 flex justify-center"
+              aria-label="Manage your account"
+            >
               {tabs.map((tab) => (
                 <Trigger
                   key={tab.id}
-                  className={`px-4 ${tab.value === currentValue ? active : inactive}`}
+                  className={`px-4 ${
+                    tab.value === currentValue ? active : inactive
+                  }`}
                   value={tab.value}
                 >
                   {tab.title}
@@ -25,7 +65,11 @@ export const Tab = () => {
               ))}
             </List>
             {tabs.map((tab) => (
-              <Content className="px-4 text-center" value={tab.value} key={tab.id}>
+              <Content
+                className="px-4 text-center"
+                value={tab.value}
+                key={tab.id}
+              >
                 <p className="italic text-amber-800 text-opacity-70 flex justify-center text-lg p-4">
                   {tab.description}
                 </p>
@@ -43,9 +87,9 @@ export const Tab = () => {
                         className="m-5"
                         width="100%"
                         height="100%"
-                        src={tab.youtube}
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        src={tab.video}
+                        title="video player"
+                        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowFullScreen
                       ></iframe>
                     </div>
@@ -56,12 +100,16 @@ export const Tab = () => {
                         key={link.description}
                         className="m-4 flex justify-center list-none w-1/3 rounded border-2 border-cyan-900 text-opacity-90 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-cyan-900 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-teal-500 hover:bg-opacity-10 hover:text-stone-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                       >
-                        <a href={link.link} className="text-cyan-900 text-opacity-90">
+                        <a
+                          href={link.link}
+                          className="text-cyan-900 text-opacity-90"
+                        >
                           {link.description}
                         </a>
                       </li>
                     ))}
                   </div>
+                  {website}
                 </div>
               </Content>
             ))}
